6e2e8a3d1fed2bd69872efe4ae86071aae65c74f
Jookeret <s5935512034@phuket.psu.ac.th>
2019-09-08 23:17:19 +0700

    route edit and add

--- redirect/src/App.js
+++ redirect/src/App.js
@@ -3,6 +3,8 @@ import React,{Component} from 'react';
 import { BrowserRouter,Route, Switch } from 'react-router-dom'
 import Login from './components/login'
 import Home from './components/home'
+import Edit from './components/edit'
+import Add from './components/add'
 
 class App extends Component {
   
@@ -17,6 +19,14 @@ class App extends Component {
           <Route
           exact path = '/'
           component={Home}
+          />
+          <Route
+          exact path = '/edit'
+          component={Edit}
+          />
+            <Route
+          exact path = '/add'
+          component={Add}
           />
           <Route/>
         </Switch>
